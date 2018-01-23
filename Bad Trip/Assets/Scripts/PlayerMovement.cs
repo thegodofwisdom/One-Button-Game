1955fea58bf3301b9dea96e8a861a7d37b657bc4
thegodofwisdom <35643511+thegodofwisdom@users.noreply.github.com>
2018-01-23 03:15:18 -0500

    Finished basic jumping
    finished foward movement, jumping, and jumping only when grounded.

--- Bad Trip/Assets/Scripts/PlayerMovement.cs
+++ Bad Trip/Assets/Scripts/PlayerMovement.cs
@@ -10,17 +10,24 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 2.0f;
     private float vertVelocity = 5.0f;
     private float gravity = 12.0f;
+    private Collider2D playerCollider;
+
+    public bool grounded;
+    public LayerMask isGround;
 
     // Use this for initialization
     void Start () {
         controller = GetComponent<CharacterController>();
         myRigidBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        grounded = Physics2D.IsTouchingLayers(playerCollider, isGround);
+
         myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, vertVelocity);
         }
